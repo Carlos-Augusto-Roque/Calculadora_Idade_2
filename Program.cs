341dfe27d8ab3b05542137a41164a300d5972198
carlos <carlosacgr@gmail.com>
2020-11-12 21:35:27 -0300

    projeto cálculo da idade exercício 2

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Calculadora_Idade_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variáveis
+
+            float anoNascimento, anoAtual, idade, idadeSemana;
+
+            //entrada de dados
+            Console.Write(" Digite o ano de nascimento ");
+            anoNascimento = float.Parse(Console.ReadLine());
+
+            Console.Write(" Digite o ano atual ");
+            anoAtual = float.Parse(Console.ReadLine());
+
+            //processamento
+
+            idade = (anoAtual - anoNascimento);
+            Console.WriteLine("A idade é de: "+idade+ " anos ");
+
+            idadeSemana = (idade*48);
+            Console.WriteLine("A idade em semanas é de: "+idadeSemana+ " semanas ");
+
+
+        }
+    }
+}
